9eec89a579df449c2181953ff36e7bafafbe3c03
John Petro <john@johnpetro.com>
2021-02-27 18:32:01 -0500

    fixed the yum clean in teh clean.sh.  The syntax was incorrect

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -8,7 +8,7 @@ if [ -d /home/vagrant/.ansible ]; then
     sudo rm -rf /home/vagrant/.ansible
 fi
 
-sudo yum clean
+sudo yum clean all
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
