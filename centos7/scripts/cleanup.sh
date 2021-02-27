12c9a4fdd3edaf059b6a9045ce55be72c5bc7bde
John Petro <john@johnpetro.com>
2021-02-27 16:53:50 -0500

    removed the writing zeros.  I am not sure if it really buys anything or not.

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -4,8 +4,8 @@
 yum -y remove ansible
 
 # Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+# dd if=/dev/zero of=/EMPTY bs=1M
+# rm -f /EMPTY
 
-# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
-sync
+# # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+# sync
