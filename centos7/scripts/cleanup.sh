51b334aed0c671e18fcba24437f590b04bff8e9b
John Petro <john@johnpetro.com>
2021-02-27 18:21:04 -0500

    changed around some things, and testing.  Looks like the copying zeros does actually help with the box compression

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -3,9 +3,15 @@
 # Remove Ansible and its dependencies.
 yum -y remove ansible
 
+if [ -d /home/vagrant/.ansible ]; then
+    echo "removing /home/vagrant/.ansible"
+    sudo rm -rf /home/vagrant/.ansible
+fi
+
+sudo yum clean
 # Zero out the rest of the free space using dd, then delete the written file.
-# dd if=/dev/zero of=/EMPTY bs=1M
-# rm -f /EMPTY
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
 
 # # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
-# sync
+sync
